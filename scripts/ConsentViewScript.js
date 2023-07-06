f5753b6019fd1c1c14368daae16905e84e2df39d
NaviteLogger <kacprzakmarek92@gmail.com>
2023-07-06 14:03:12 +0200

    Linked all .js files to current .html files

---
+++ scripts/ConsentViewScript.js
@@ -0,0 +1,18 @@
+function submitForm() {
+    var name = document.getElementById('nameInput').value;
+    var surname = document.getElementById('surnameInput').value;
+    
+    // Perform any additional client-side validation if needed
+    
+    // Send the form data to the server for processing
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/submit-form', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        // Success! Do something with the response if needed
+        console.log(xhr.responseText);
+      }
+    };
+    xhr.send(JSON.stringify({ name: name, surname: surname }));
+  }

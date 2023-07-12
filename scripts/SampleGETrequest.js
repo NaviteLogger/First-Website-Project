53627435763f35193a8111d5796f0106eaaadc30
NaviteLogger <kacprzakmarek92@gmail.com>
2023-07-12 13:09:24 +0200

    Added comments to EmailVerificationPage and CodeVerificationPage

--- scripts/SampleGETrequest.js
+++ scripts/SampleGETrequest.js
@@ -1,14 +1,18 @@
+/*
+ * Makes a GET request to 'https:...' and logs the response data to the console.
+ * @throws {Error} If the HTTP status code is not ok.
+ */
 fetch("https://api.example.com/data")
-  .then(function(response) {
+  .then((response) => {
     if (response.ok) {
       return response.json();
     } else {
       throw new Error("HTTP status code: " + response.status);
     }
   })
-  .then(function(data) {
+  .then((data) => {
     console.log(data);
   })
-  .catch(function(error) {
+  .catch((error) => {
     console.log(error);
   });
